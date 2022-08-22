0e9125c2afddc78575c064f9874adc1f9eccf71c
Geremias Santos <Geremias99999@gmail.com>
2022-08-22 16:38:33 -0300

    Sum Array

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Sobre o Mini- Projeto
+
+O projeto se resume em retornar em console log a soma dos elementos de um array, sendo eles todos inteiros. 
+
+## Linguagens Utilizadas
+
+- JavaScript
---
+++ script.js
@@ -0,0 +1,11 @@
+const myArr = [2, 3, 4, 5, 6];
+
+let total = 0
+
+//Laço aonde realiza a soma de todos os elemento do array
+for(let  i = 0; i < myArr.length; i++){
+   total += myArr[i]
+}
+
+console.log(total)
+
